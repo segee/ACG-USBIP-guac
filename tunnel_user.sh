082ecada35f3dea0129edfff581dd97b57f338bb
Bruce Segee <segee@maine.edu>
2018-02-11 13:11:45 -0500

    made a restricted authorized_keys file

--- tunnel_user.sh
+++ tunnel_user.sh
@@ -7,4 +7,9 @@ sudo mkdir /home/tunnel/.ssh
 # copy an appropriate authorized_keys file
 # see https://stackoverflow.com/questions/8021/allow-user-to-set-up-an-ssh-tunnel-but-nothing-else
 
+sudo chown tunnel ~tunnel/.ssh
 sudo cp /usr/local/bin/ACG-USBIP-guac/authorized_keys ~tunnel/.ssh/.
+sudo chown tunnel ~tunnel/.ssh/authorized_keys
+sudo chmod 700 ~tunnel/.ssh
+sudo chmod 700 ~tunnel/.ssh/authorized_keys
+
